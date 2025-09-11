f22451ed78cbfe3dce96c616174fe4ec5a757af1
Adam Pingel <oss@pingel.org>
2025-09-11 11:50:09 -0400

    preliminary about page
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- apps/frontend/src/components/Footer.tsx
+++ apps/frontend/src/components/Footer.tsx
@@ -18,6 +18,12 @@ export function Footer() {
             
             <div className="flex space-x-6 text-sm">
               <Link 
+                href="/about" 
+                className="text-gray-500 hover:text-gray-700 transition-colors"
+              >
+                About
+              </Link>
+              <Link 
                 href="/privacy" 
                 className="text-gray-500 hover:text-gray-700 transition-colors"
               >
