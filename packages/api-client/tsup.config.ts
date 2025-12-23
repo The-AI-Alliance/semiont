4f43130545dc675312f049198eeaa0e9476a6966
Adam Pingel <oss@pingel.org>
2025-12-22 23:54:52 -0500

    convert backend to ESM
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- packages/api-client/tsup.config.ts
+++ packages/api-client/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/utils/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   dts: true,
   clean: true,
   sourcemap: true,
