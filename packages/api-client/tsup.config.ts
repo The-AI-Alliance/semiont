2f1c15c1cade91a5eb69ff37c4c25fc19162d4c2
Adam Pingel <oss@pingel.org>
2025-12-22 23:44:24 -0500

    dual ESM/CJS builds to @semiont/core for backend
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- packages/api-client/tsup.config.ts
+++ packages/api-client/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/utils/index.ts'],
-  format: ['esm'],
+  format: ['esm', 'cjs'],
   dts: true,
   clean: true,
   sourcemap: true,
