8a6e62dc21285345242f2d1e24040a7caaf29931
Adam Pingel <oss@pingel.org>
2026-03-06 15:35:03 -0500

    avoid test split
    Signed-off-by: Adam Pingel <oss@pingel.org>

--- packages/react-ui/vitest.config.ts
+++ packages/react-ui/vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     setupFiles: ['./vitest.setup.ts'],
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'json', 'json-summary', 'html'],
+      reporter: ['text', 'json', 'json-summary', 'html', 'lcov'],
       exclude: [
         'node_modules/',
         '**/*.d.ts',
